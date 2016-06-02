b221a8a972dd1449507322982b0f6309f51b4ecd
Kelvin Watson <watsokel@oregonstate.edu>
2016-06-02 15:38:10 -0700

    toggle
    toggle

--- app/src/main/java/com/watsonlogic/eventsapp/EditFragment.java
+++ app/src/main/java/com/watsonlogic/eventsapp/EditFragment.java
@@ -1,14 +1,20 @@
 package com.watsonlogic.eventsapp;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.text.TextUtilsCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
+public class EditFragment extends Fragment implements View.OnClickListener {
 
-public class EditFragment extends Fragment {
+    private EditText username;
+    private Button submitButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -17,4 +23,29 @@ public class EditFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_edit, container, false);
     }
 
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getWidgets();
+    }
+
+    private void getWidgets(){
+        username = (EditText)getActivity().findViewById(R.id.username_edit_text);
+        submitButton = (Button)getActivity().findViewById(R.id.submit_button);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.submit_button:{
+                validateInput();
+                break;
+            }
+        }
+    }
+
+    private void validateInput(){
+
+    }
 }
